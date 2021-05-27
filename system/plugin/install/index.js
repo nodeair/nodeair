b42fa793fd67defa046a619c09658875ea0b8d58
lisniuse <17560235@qq.com>
2021-05-27 22:34:01 +0800

    feat: 更新安装界面样式

--- system/plugin/install/index.js
+++ system/plugin/install/index.js
@@ -17,37 +17,34 @@ async function checkInstall() {
 
 async function loaded() {
   const { koaApp, koaRouter, lang, router, hook } = this;
-  // 注册插件所使用的多语言文案
-  lang.register(langConf.call(this));
-
   const app = this;
   const isInstalled = await checkInstall.call(this);
 
-  if (!isInstalled) {
-    const state = {
-      router: routerConfig.call(app),
-      async handler404(ctx, next) {
-        try {
-          await next();
-          if (!ctx.body) {
-            await notInstalledController.apply(app, [ctx, next]);
-          }
-        } catch (e) {
-          ctx.body = '500'
-        }
+  if (isInstalled) return;
+  // 注册插件所使用的多语言文案
+  lang.register(langConf.call(this));
+
+  const state = {
+    router: routerConfig.call(app),
+    async handler404(ctx, next) {
+      try {
+        await next();
+        if (!ctx.body) await notInstalledController.apply(app, [ctx, next]);
+      } catch (e) {
+        ctx.body = '500';
       }
     }
+  }
 
-    // 调用钩子
-    await hook.emit('core.install.01', state);
+  // 调用钩子
+  await hook.emit('core.install.01', state);
 
-    // 处理404情况
-    koaApp.use(state.handler404)
+  // 处理404情况
+  koaApp.use(state.handler404)
 
-    // 循环注册路由
-    for (const item of state.router) {
-      router.push(item.type, item.url, item.controller);
-    }
+  // 循环注册路由
+  for (const item of state.router) {
+    router.push(item.type, item.url, item.controller);
   }
 }
 
