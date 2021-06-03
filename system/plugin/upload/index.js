fd7ba82660dac5524b2aaaa48983eee0e6a23f2c
lisniuse <17560235@qq.com>
2021-06-03 23:16:14 +0800

    feat: 调整mock插件简介；删除upload插件多余注释

--- system/plugin/upload/index.js
+++ system/plugin/upload/index.js
@@ -4,14 +4,6 @@ async function loaded() {
   log.system('加载 upload 插件');
   // 监听 upload 文件夹
   staticServer.register(UPLOAD_DIR, '/upload');
-  // koa.app.use(async function (ctx, next) {
-  //   // await next();
-  //   const temp = ctx.path.substr(0, UPLOAD_NAME.length + 1);
-  //   if (temp === `/${UPLOAD_NAME}`) {
-  //     const staticMiddleware = await koa.static(ROOT);
-  //     await staticMiddleware(ctx, next);
-  //   }
-  // });
 }
 
 module.exports = loaded;
